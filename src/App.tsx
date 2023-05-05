c62b01b51fed142a5e0095d3fa028d83208d1fa5
Maksym Kuzmych <kuzmichmaksym1997@gmail.com>
2023-05-05 15:52:33 +0300

    feat: add error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-import './App.css';
+import { ErrorBoundary } from 'react-error-boundary';
+
+import './App.scss';
+import { Error } from './components/Error/Error';
 
 export const App = () => {
-  return <div className='App'></div>;
+  return (
+    <ErrorBoundary fallback={<Error />}>
+      <div className='App'></div>
+    </ErrorBoundary>
+  );
 };
